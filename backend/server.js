d565b28ce40a7dda26105ef239a4238e15a5deff
Ethan Zimbelman <itszimbo@gmail.com>
2021-05-01 13:04:30 -0700

    jwt make + check

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const ejwt = require('express-jwt');
+const jsonwebtoken = require('jsonwebtoken');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 const routes = require('./routes');
 
 const app = express();
+
+const secret = 'JWT_SECRET_KEY_HERE';
+const ejwt_options = { secret, algorithms: ['sha1', 'RS256', 'HS256'] };
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
@@ -22,8 +27,16 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
-
 app.use('/', routes);
+
+app.post('/auth', (req, res) => {
+  const token = jsonwebtoken.sign({ foo: 'bar' }, secret);
+  res.send(token);
+});
+
+app.get('/protected', ejwt(ejwt_options), (req, res) => {
+  res.json(req.user);
+});
 
 if (process.argv.includes('dev')) {
   const PORT = process.env.PORT || 3001;

899ff5931b16ba4db97f6e83f40bdcd70833c6f3
Ethan Zimbelman <itszimbo@gmail.com>
2021-05-04 13:31:50 -0700

    CORS: allow credentials

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,7 @@ mongoose.connect(process.env.DATABASE_URL, {
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", true);
   res.header("Referrer-Policy", "same-origin");
   next();
 });
